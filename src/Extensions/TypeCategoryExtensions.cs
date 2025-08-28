edb4f516b2dbd880425e36f0f8b6be575f9e0f09
PDempsey03 <dempsey.202@osu.edu>
2025-08-28 14:48:13 -0400

    Added ability to convert type category to is nullable version

---
+++ src/Extensions/TypeCategoryExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace MmcSerializer.Extensions
+{
+    public static class TypeCategoryExtensions
+    {
+        public static TypeCategory ToNullable(this TypeCategory typeCategory)
+        {
+            return typeCategory switch
+            {
+                TypeCategory.Primitive => TypeCategory.NullablePrimitive,
+                TypeCategory.Enum => TypeCategory.NullableEnum,
+                TypeCategory.Struct => TypeCategory.NullableStruct,
+                _ => typeCategory
+            };
+        }
+    }
+}
