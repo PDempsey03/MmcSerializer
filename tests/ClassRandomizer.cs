5a3dd72bdfe31132ff8a544edcc34a5b6de01fa0
PDempsey03 <123753503+PDempsey03@users.noreply.github.com>
2025-08-28 16:19:07 -0400

    Primitive implementation
    Implemented the ability to serialize primitives inside a class.

---
+++ tests/ClassRandomizer.cs
@@ -0,0 +1,62 @@
+﻿using System.Reflection;
+
+namespace MmcSerializer.Tests
+{
+    public static class ClassRandomizer
+    {
+        public static void RandomizeClassFieldAndPropertyPrimitives<T>(T obj) where T : class
+        {
+            Random rng = new Random();
+
+            var fields = typeof(T).GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            foreach (var field in fields)
+            {
+                object value = field.FieldType switch
+                {
+                    Type t when t == typeof(bool) => rng.Next(2) == 0,
+                    Type t when t == typeof(byte) => (byte)rng.Next(byte.MinValue, byte.MaxValue + 1),
+                    Type t when t == typeof(sbyte) => (sbyte)rng.Next(sbyte.MinValue, sbyte.MaxValue + 1),
+                    Type t when t == typeof(char) => (char)rng.Next(char.MinValue + 0xA, 0xD700), // avoid unicode surrogate issues
+                    Type t when t == typeof(double) => rng.NextDouble() * 1000 - 500,
+                    Type t when t == typeof(float) => (float)(rng.NextDouble() * 1000 - 500),
+                    Type t when t == typeof(int) => rng.Next(int.MinValue, int.MaxValue),
+                    Type t when t == typeof(uint) => (uint)rng.Next(int.MinValue, int.MaxValue),
+                    Type t when t == typeof(nint) => (nint)rng.Next(int.MinValue, int.MaxValue),
+                    Type t when t == typeof(nuint) => (nuint)rng.Next(int.MinValue, int.MaxValue),
+                    Type t when t == typeof(long) => (long)(rng.NextDouble() * long.MaxValue),
+                    Type t when t == typeof(ulong) => (ulong)(rng.NextDouble() * ulong.MaxValue),
+                    Type t when t == typeof(short) => (short)rng.Next(short.MinValue, short.MaxValue + 1),
+                    Type t when t == typeof(ushort) => (ushort)rng.Next(ushort.MinValue, ushort.MaxValue + 1),
+                    _ => field.GetValue(obj)! // if not primitive type, leave untouched
+                };
+
+                field.SetValue(obj, value);
+            }
+
+            var properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            foreach (var property in properties)
+            {
+                object value = property.PropertyType switch
+                {
+                    Type t when t == typeof(bool) => rng.Next(2) == 0,
+                    Type t when t == typeof(byte) => (byte)rng.Next(byte.MinValue, byte.MaxValue + 1),
+                    Type t when t == typeof(sbyte) => (sbyte)rng.Next(sbyte.MinValue, sbyte.MaxValue + 1),
+                    Type t when t == typeof(char) => (char)rng.Next(char.MinValue + 0xA, 0xD700), // avoid unicode surrogate issues
+                    Type t when t == typeof(double) => rng.NextDouble() * 1000 - 500,
+                    Type t when t == typeof(float) => (float)(rng.NextDouble() * 1000 - 500),
+                    Type t when t == typeof(int) => rng.Next(int.MinValue, int.MaxValue),
+                    Type t when t == typeof(uint) => (uint)rng.Next(int.MinValue, int.MaxValue),
+                    Type t when t == typeof(nint) => (nint)rng.Next(int.MinValue, int.MaxValue),
+                    Type t when t == typeof(nuint) => (nuint)rng.Next(int.MinValue, int.MaxValue),
+                    Type t when t == typeof(long) => (long)(rng.NextDouble() * long.MaxValue),
+                    Type t when t == typeof(ulong) => (ulong)(rng.NextDouble() * ulong.MaxValue),
+                    Type t when t == typeof(short) => (short)rng.Next(short.MinValue, short.MaxValue + 1),
+                    Type t when t == typeof(ushort) => (ushort)rng.Next(ushort.MinValue, ushort.MaxValue + 1),
+                    _ => property.GetValue(obj)! // if not primitive type, leave untouched
+                };
+
+                property.SetValue(obj, value);
+            }
+        }
+    }
+}
---
+++ tests/Models/Primitive/MultiPrimitive.cs
@@ -0,0 +1,150 @@
+﻿using System.Reflection;
+
+namespace MmcSerializer.Tests.Models.Primitive
+{
+    public class MultiPrimitive
+    {
+        public bool _boolField;
+
+        public byte _byteField;
+
+        public sbyte _sbyteField;
+
+        public char _charField;
+
+        public double _doubleField;
+
+        public float _floatField;
+
+        public int _intField;
+
+        public uint _uintField;
+
+        public nint _nintField;
+
+        public nuint _nuintField;
+
+        public long _longField;
+
+        public ulong _ulongField;
+
+        public short _shortField;
+
+        public ushort _ushortField;
+
+        private bool _privateBoolField;
+
+        private byte _privateByteField;
+
+        private sbyte _privateSbyteField;
+
+        private char _privateCharField;
+
+        private double _privateDoubleField;
+
+        private float _privateFloatField;
+
+        private int _privateIntField;
+
+        private uint _privateUintField;
+
+        private nint _privateNintField;
+
+        private nuint _privateNuintField;
+
+        private long _privateLongField;
+
+        private ulong _privateUlongField;
+
+        private short _privateShortField;
+
+        private ushort _privateUshortField;
+
+        public bool BoolProperty { get; set; }
+
+        public byte ByteProperty { get; set; }
+
+        public sbyte SbyteProperty { get; set; }
+
+        public char CharProperty { get; set; }
+
+        public double DoubleProperty { get; set; }
+
+        public float FloatProperty { get; set; }
+
+        public int IntProperty { get; set; }
+
+        public uint UintProperty { get; set; }
+
+        public nint NintProperty { get; set; }
+
+        public nuint NuintProperty { get; set; }
+
+        public long LongProperty { get; set; }
+
+        public ulong UlongProperty { get; set; }
+
+        public short ShortProperty { get; set; }
+
+        public ushort UshortProperty { get; set; }
+
+        private bool PrivateBoolProperty { get; set; }
+
+        private byte PrivateByteProperty { get; set; }
+
+        private sbyte PrivateSbyteProperty { get; set; }
+
+        private char PrivateCharProperty { get; set; }
+
+        private double PrivateDoubleProperty { get; set; }
+
+        private float PrivateFloatProperty { get; set; }
+
+        private int PrivateIntProperty { get; set; }
+
+        private uint PrivateUintProperty { get; set; }
+
+        private nint PrivateNintProperty { get; set; }
+
+        private nuint PrivateNuintProperty { get; set; }
+
+        private long PrivateLongProperty { get; set; }
+
+        private ulong PrivateUlongProperty { get; set; }
+
+        private short PrivateShortProperty { get; set; }
+
+        private ushort PrivateUshortProperty { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is null || obj is not MultiPrimitive other) return false;
+
+            // use reflection to auto check every property rather than manually checking everything
+
+            var fields = GetType().GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            foreach (var field in fields)
+            {
+                var thisValue = field.GetValue(this);
+                var otherValue = field.GetValue(other);
+
+                if (!Equals(thisValue, otherValue))
+                    return false;
+            }
+
+            var properties = GetType().GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            foreach (var prop in properties)
+            {
+                if (!prop.CanRead) continue;
+
+                var thisValue = prop.GetValue(this);
+                var otherValue = prop.GetValue(other);
+
+                if (!Equals(thisValue, otherValue))
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
